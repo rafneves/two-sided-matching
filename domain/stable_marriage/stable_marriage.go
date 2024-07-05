5a9409d44b9c3d8d45105e1548198932ac6ed6e9
Rafael Neves <rafaelneves@gmail.com>
2024-07-05 00:07:49 -0300

    Add signature for Stable Marriage Matching

--- domain/stable_marriage/stable_marriage.go
+++ domain/stable_marriage/stable_marriage.go
@@ -1,1 +1,17 @@
 package stable_marriage
+
+import "github.com/rafneves/two-sided-matching/domain/entities"
+
+type StableMarriage struct {
+}
+
+type FindMatchingInput struct {
+	Men   []*entities.Man
+	Women []*entities.Woman
+}
+
+type FindMatchingOutput entities.Matching
+
+func (m *StableMarriage) FindMatching(*FindMatchingInput) (*FindMatchingOutput, error) {
+	return &FindMatchingOutput{}, nil
+}

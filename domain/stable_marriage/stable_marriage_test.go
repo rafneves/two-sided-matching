59f9c6e02cee95cdde05ed32b2c753a6ae9a86a7
Rafael Neves <rafaelneves@gmail.com>
2024-07-06 15:38:42 -0300

    Add a test case for Stable Marriage
    It still fails as the function is not implemented yet.

---
+++ domain/stable_marriage/stable_marriage_test.go
@@ -0,0 +1,78 @@
+package stable_marriage_test
+
+import (
+	"github.com/rafneves/two-sided-matching/domain/entities"
+	"github.com/rafneves/two-sided-matching/domain/stable_marriage"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestStableMarriage_FindMatching(t *testing.T) {
+	// Initialization of People
+	// Men
+	anatole := &entities.Man{ID: "anatole"}
+	barnabe := &entities.Man{ID: "barnabe"}
+	camille := &entities.Man{ID: "camille"}
+	dominique := &entities.Man{ID: "dominique"}
+
+	// Women
+	antoniette := &entities.Woman{ID: "antoniette"}
+	brigitte := &entities.Woman{ID: "brigitte"}
+	cunegonde := &entities.Woman{ID: "cunegonde"}
+	donatienne := &entities.Woman{ID: "donatienne"}
+
+	// Men Preference
+	anatole.Preference = []*entities.Woman{cunegonde, brigitte, donatienne, antoniette}
+	barnabe.Preference = []*entities.Woman{brigitte, antoniette, cunegonde, donatienne}
+	camille.Preference = []*entities.Woman{brigitte, donatienne, antoniette, cunegonde}
+	dominique.Preference = []*entities.Woman{brigitte, donatienne, antoniette, cunegonde}
+
+	// Women Preference
+	antoniette.Preference = []*entities.Man{anatole, barnabe, dominique, anatole}
+	brigitte.Preference = []*entities.Man{camille, anatole, dominique, camille}
+	cunegonde.Preference = []*entities.Man{camille, barnabe, dominique, anatole}
+	donatienne.Preference = []*entities.Man{barnabe, anatole, camille, dominique}
+
+	cases := []struct {
+		name           string
+		input          *stable_marriage.FindMatchingInput
+		expectedResult *stable_marriage.FindMatchingOutput
+		err            error
+	}{
+		{
+			input: &stable_marriage.FindMatchingInput{
+				[]*entities.Man{anatole, barnabe, camille, dominique},
+				[]*entities.Woman{antoniette, brigitte, cunegonde, donatienne},
+			},
+			expectedResult: &stable_marriage.FindMatchingOutput{
+				Couples: []*entities.Couple{
+					{
+						Man:   *anatole,
+						Woman: *donatienne,
+					},
+					{
+						Man:   *barnabe,
+						Woman: *antoniette,
+					},
+					{
+						Man:   *camille,
+						Woman: *brigitte,
+					},
+					{
+						Man:   *dominique,
+						Woman: *cunegonde,
+					},
+				},
+			},
+			err: nil,
+		},
+	}
+
+	for _, c := range cases {
+		matching := stable_marriage.StableMarriage{}
+		result, err := matching.FindMatching(c.input)
+
+		require.Equal(t, c.expectedResult, result)
+		require.Equal(t, c.err, err)
+	}
+}
